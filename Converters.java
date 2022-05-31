12871fa46f0f80084f55ecd7c12c158ca6d69062
tginator <gordontyler11@gmail.com>
2022-06-01 03:47:52 +0800

    Working function3 1.0

--- Converters.java
+++ Converters.java
@@ -1,7 +1,7 @@
 import java.util.*;
+import java.util.Scanner;
 
-public class Converters
-{
+public class Converters {
 
     public static void main(String[] args) {
 
@@ -11,37 +11,47 @@ public class Converters
 
         System.out.println("Please choose a function: ");
         System.out.println("1. Convert text to upper case or lower case");
+        System.out.println("2. Identify whether numeric values are in a given string.");
+        System.out.println("3. Identify whether a given string is a valid number or not.");
+
 
         choice = sc.nextInt();
 
-        if (choice == 1 );
-        {
+        if (choice == 1) {
             System.out.println("Please choose to convert to upper or lower case: ");
             System.out.println("1. Lower case to upper case.");
             System.out.println("2. Upper case to lower case.");
+
             int select;
             select = sc.nextInt();
 
-            if (select == 1);
-            {
+            if (select == 1) {
                 String txt;
                 txt = lowerCase();
 
                 System.out.println(txt.toUpperCase());
             }
 
-            if (select == 2);
-            {
-                String txt;
-                txt = upperCase();
+            if (select == 2) {
+                String txt2;
+                txt2 = upperCase();
 
-                System.out.println(txt.toLowerCase());
+                System.out.println(txt2.toLowerCase());
             }
+        }
+        if (choice == 2) {
+
+            containNumeric();
+
+        }
+
+        if (choice == 3) {
+
+            isNumeric();
         }
     }
 
-    public static String lowerCase()
-    {
+    public static String lowerCase() {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Please enter a text that you wish to be converted to upper case.");
@@ -50,20 +60,67 @@ public class Converters
         txt = sc.nextLine();
         return txt;
 
-
     }
 
-    public static String upperCase()
-    {
+    public static String upperCase() {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Please enter a text that you wish to be converted to lower case: ");
 
 
-        String txt;
-        txt = sc.nextLine();
-        return txt;
+        String txt2;
+        txt2 = sc.nextLine();
+        return txt2;
 
     }
+
+    public static void containNumeric() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter string: ");
+        String input;
+        input = sc.next();
+
+        char[] chars = input.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char c : chars) {
+            if (Character.isDigit(c)) {
+                sb.append(c);
+            }
+        }
+        boolean numeric = true;
+        try {
+            Double num = Double.parseDouble(String.valueOf(sb));
+        } catch (NumberFormatException e) {
+            numeric = false;
+        }
+        if (numeric)
+            System.out.println(input + " contains a number");
+        else
+            System.out.println(input + " doesn't contain a number");
+    }
+
+    public static void isNumeric()
+    {
+        Scanner sc = new Scanner(System.in);
+
+
+        String input = sc.next();
+        boolean numeric = true;
+
+        try {
+            Double num = Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            numeric = false;
+        }
+
+        if(numeric)
+            System.out.println(input + " is a valid number");
+        else
+            System.out.println(input + " is not a valid number");
+    }
 }
+
+
+
 
