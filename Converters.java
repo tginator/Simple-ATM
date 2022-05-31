bdee051c440edcb9353e3b69cad5eb3c895cc90d
tginator <gordontyler11@gmail.com>
2022-05-31 14:46:31 +0800

    implement function 1

--- Converters.java
+++ Converters.java
@@ -2,25 +2,68 @@ import java.util.*;
 
 public class Converters
 {
+
     public static void main(String[] args) {
 
-        System.out.println("Please enter a text that you wish to be converted to upper case.");
+        Scanner sc = new Scanner(System.in);
 
-        String txt;
-        txt = lowerCase();
+        int choice;
 
+        System.out.println("Please choose a function: ");
+        System.out.println("1. Convert text to upper case or lower case");
 
-        System.out.print(txt.toUpperCase());
+        choice = sc.nextInt();
+
+        if (choice == 1 );
+        {
+            System.out.println("Please choose to convert to upper or lower case: ");
+            System.out.println("1. Lower case to upper case.");
+            System.out.println("2. Upper case to lower case.");
+            int select;
+            select = sc.nextInt();
+
+            if (select == 1);
+            {
+                String txt;
+                txt = lowerCase();
+
+                System.out.println(txt.toUpperCase());
+            }
+
+            if (select == 2);
+            {
+                String txt;
+                txt = upperCase();
+
+                System.out.println(txt.toLowerCase());
+            }
+        }
     }
 
     public static String lowerCase()
     {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Please enter a text that you wish to be converted to upper case.");
+
         String txt;
         txt = sc.nextLine();
         return txt;
+
+
     }
 
+    public static String upperCase()
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter a text that you wish to be converted to lower case: ");
+
+
+        String txt;
+        txt = sc.nextLine();
+        return txt;
+
+    }
 }
 
